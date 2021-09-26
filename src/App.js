89c61eb58c4c6375bc97bd4a9913a02a4edfa697
Talal <talalstudent@gmail.com>
2021-09-26 13:02:07 +0500

    Added Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import Navbar from './components/Navbar';
 import About from './components/About';
-
+import { useState } from 'react';
 import './App.css';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState('light')
+
+  const toggleMode = ()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#092036';
+    }else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
   return (
     <>
-      <Navbar title="Text Utils"/>
+      <Navbar title="Text Utils" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-3">
-      <About/>
+      <TextForm mode={mode} />
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,48 +1,75 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-export default function TextForm() {
+export default function TextForm(props) {
+  const [text, setText] = useState("");
 
-    const [text,setText] = useState("");
+  const handleOnChange = (e) => {
+    setText(e.target.value);
+  };
 
-    const handleOnChange = (e) =>{
-        setText(e.target.value)
-    }
+  const handleUpper = () => {
+    const newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleLower = () => {
+    const newText = text.toLowerCase();
+    setText(newText);
+  };
 
-    const handleUpper = () =>{
+  const handleClear = () => {
+    setText("");
+  };
 
-        const newText = text.toUpperCase();
-        setText(newText) 
-    }
-    const handleLower = () =>{
-
-        const newText = text.toLowerCase();
-        setText(newText) 
-    }
-
-    const handleClear = () =>{
-        setText("")
-    }
-
-    return (
-        <>
-        <div className="container">
-            <div className="container my-3">
-                    <div className="mb-3">
-                        <label htmlFor="text" className="form-label">Enter your Text Here</label>
-                        <textarea className="form-control" onChange={handleOnChange} value = {text} id="text" rows="8"></textarea>
-                    </div>
-                    <button className="btn btn-primary mx-1" onClick={handleUpper}>To Upper Case</button>
-                    <button className="btn btn-primary mx-1" onClick={handleLower}>To Lower Case</button>
-                    <button className="btn btn-primary mx-1" onClick={handleClear}>Clear Text</button>
-            </div>
-        </div>
+  return (
+    <>
+      <div
+        className="p-0 container"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
         <div className="container my-3">
-            <h2>Your Text Summary</h2>
-            <p>{text.split(" ").length} words and {text.length} characters</p>
-            <p>{0.008 * text.split(" ").length} Minutes read</p>
-            <h2>Preview</h2>
-            <p>{text}</p>
+          <div className="mb-3">
+            <label htmlFor="text" className="form-label">
+              Enter your Text Here
+            </label>
+            <textarea
+              className="form-control"
+              onChange={handleOnChange}
+              value={text}
+              id="text"
+              rows="8"
+              style={{
+                  backgroundColor: props.mode==='dark'?'#082036':'white',
+                  color: props.mode==='dark'?'white':'black'
+              }}
+            ></textarea>
+          </div>
+          <button className="btn btn-primary mx-1" onClick={handleUpper}>
+            To Upper Case
+          </button>
+          <button className="btn btn-primary mx-1" onClick={handleLower}>
+            To Lower Case
+          </button>
+          <button className="btn btn-primary mx-1" onClick={handleClear}>
+            Clear Text
+          </button>
         </div>
-        </>
-    )
+      </div>
+      <div
+        className=" container"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h2>Your Text Summary</h2>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>
+          {text.length > 0
+            ? text
+            : "Enter something in the text area above to preview it here"}
+        </p>
+      </div>
+    </>
+  );
 }
